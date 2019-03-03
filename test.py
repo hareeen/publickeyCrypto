f098710343033e1e9d431f11783854af36cfa3ba
himyu <himyuuuu1@gmail.com>
2019-03-04 02:52:06 +0900

    Update test

--- test.py
+++ test.py
@@ -5,6 +5,10 @@ prec = 64
 %timeit pow_simple(3, 100000, 7)
 %timeit pow_fast(3, 100000, 7)
 
+ts1 = generate_prime(50, prec)
+ts2 = generate_prime(100, prec)
+%timeit get_arithmetic_inv(ts1, ts2)
+
 test_prime = generate_prime(100, prec)
 %timeit primality_test(test_prime, prec)
 
